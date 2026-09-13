f50bca340e945b706e69917a8ed30d9523ec46b1
asimrana01 <asimrana2578@gmail.com>
2026-09-13 13:21:41 +0500

    Added General Error Handling in Tasks

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -1,58 +1,51 @@
 const taskService = require("../services/task.service")
-
+const AppError=require("../utils/AppError")
 async function createTask(req, res) {
-    try {
+
         const owner = req.user.id
         const taskData = req.body
         const task = await taskService.createTask(owner, taskData)
-        res.status(201).json({ message: "Task Created Successfully", task })
-    } catch (error) {
-        res.status(error.status || 500).json({ message: error.message })
-    }
+       return  res.status(201).json({ message: "Task Created Successfully", task })
+   
 }
 
 async function getAllTasks(req, res) {
-    try {
+    
         const ownerId = req.user.id
         const task = await taskService.findTaskByOwner(ownerId)
-        res.status(200).json({ message: "All tasks are", task })
-    } catch (error) {
-        res.status(error.status || 500).json({ message: error.message })
-    }
+        return res.status(200).json({ message: "All tasks are", task })
 }
 
 async function getSingleTask(req, res) {
-    try {
+    
         const ownerId = req.user.id
         const taskId = req.params.id
         const task = await taskService.findByIdAndOwner(taskId, ownerId)
-        res.status(200).json({ message: "Task found", task })
-    } catch (error) {
-        res.status(error.status || 500).json({ message: error.message })
+        if(!task)
+            throw new AppError("Task Not Found",404)
+    return res.status(200).json({message:"Task is As",task:task})
     }
-}
+
 
 async function updateTask(req, res) {
-    try {
         const ownerId = req.user.id
         const taskId = req.params.id
         const updates = req.body
         const task = await taskService.updateTaskByIdAndOwner(taskId, ownerId, updates)
-        res.status(200).json({ message: "Task updated Successfully", task })
-    } catch (error) {
-        res.status(error.status || 500).json({ message: error.message })
-    }
+        if(!task)
+            throw new AppError("Task not Found",404)
+        return res.status(200).json({ message: "Task updated Successfully", task })
+   
 }
 
 async function deleteTask(req, res) {
-    try {
         const taskId = req.params.id
         const ownerId = req.user.id
         const task = await taskService.deleteTaskByIdAndOwner(taskId, ownerId)
-        res.status(200).json({ message: "Task Deleted", task })
-    } catch (error) {
-        res.status(error.status || 500).json({ message: error.message })
-    }
+        if(!task)
+            throw new AppError("Task not Found",404)
+        return res.status(200).json({ message: "Task Deleted", task })
+   
 }
 
 module.exports = { createTask, getAllTasks, getSingleTask, updateTask, deleteTask }
